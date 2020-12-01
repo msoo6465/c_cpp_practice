a40dd810acdb6fce70cb2aed8bc64349d7f29bc1
Minsu <msoo6465@gmail.com>
2020-12-01 23:10:46 +0900

    Add 02-6 Reference

---
+++ cpp_basic/02/02-6/RefReturnOne.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int& RefRetFuncOne(int& ref) {
+	ref++;
+	return ref;
+}
+
+int RefRetFunc(void) {
+	int num1 = 1;
+	int& num2 = RefRetFuncOne(num1);
+
+	
+	num1++;
+	cout << "num1 : " << num1 << endl;
+	cout << "num2 : " << num2 << endl;
+	num2++;
+
+	cout << "num1 : " << num1 << endl;
+	cout << "num2 : " << num2 << endl;
+
+	return 0;
+}
